71e2801ff0920c6981310ec3b38620b2791b5858
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2026-02-25 02:09:20 +0530

    second

--- index.js
+++ index.js
@@ -109,6 +109,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(process.env.PORT || 8000, "127.0.0.1", () => {
   console.log("server started...");
 });
